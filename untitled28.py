0f913f079489e7f25aab236bd06f9414907799ee
João Victor <joaovictor.1973@hotmail.com>
2023-02-11 16:59:25 -0300

    atualizando arquivo com filtro de preços em $

--- untitled28.py
+++ untitled28.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-import json
+import locale
 
 dados = pd.read_csv('housing.csv')
 df = dados.copy()
@@ -32,16 +32,17 @@ st.title("Análise do Preço por Região\n")
 st.write("Vamos ver como o preço de casas na Califórnia varia por região.")
 
 # Filtros para a tabela
-checkbox_mostrar_tabela = st.sidebar.checkbox('Mostrar tabela') # Se estiver selecionado no streamlit
+checkbox_mostrar_tabela = st.sidebar.checkbox('Mostrar tabela', value = True) # Se estiver selecionado no streamlit
 if checkbox_mostrar_tabela:
 
     st.sidebar.markdown('## Filtro para a tabela') # mostra as possibilidades de filtro (near bay, the ocean, inland, near ocean, island)
-
     categorias = list(df["Proximidade do mar"].unique()) #cria uma opção com todas as categorias
     categorias.append('Todas') # Adiciona todas, para caso queira ver todas as regiões pintadas no streamlit depois
-
     categoria = st.sidebar.selectbox('Selecione a região para visualizar o gráfico',
                                      options = categorias)
+
+
+
     # 1º Filtrar os dados
     # 2º Fazer figura de acordo com o dado filtrado
     # 3º Aplicar ao streamlit
@@ -51,18 +52,30 @@ if checkbox_mostrar_tabela:
     else:
         pass
 
+    st.sidebar.markdown('## Defina o Preço da Casa')
+    valor_min, valor_max = st.sidebar.slider("Escolha o intervalo de valores", 0, 500003, (0, 500003), step=5)
+
+    # Mostrar o valor selecionado na sidebar
+    st.sidebar.markdown(f"Valor mínimo: $ {valor_min:,.0f}")
+    st.sidebar.markdown(f"Valor máximo: $ {valor_max:,.0f}")
+
+    df = df[(df['valor mediano da casa'] >= valor_min) & (df['valor mediano da casa'] <= valor_max)]
+
     # px.scatter_mapbox é um comando que permite visualizar um gráfico de distribuição em forma de caixa
     # 1º especifiquei o dataset df
     # 2º após relacionei lat e lon com a primeira e segunda coluna do df
     # 3º color='Proximidade do mar', permite com que de acordo com a localidade que para o nosso caso são 5, faça uma cor pra cada local
     # 4º mapbox_style, permite a utilização do mapa aberto
 
-    df['valor_mediano_da_casa'] = 'R$ ' + df['valor mediano da casa'].astype(str)
+    df['valor mediano da casa'] = df['valor mediano da casa'].apply(lambda x: "${:,.0f}".format(x))
 
     colors = ['purple', 'gray', 'pink', 'red', 'green'] # lista de cores
-    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color = "Proximidade do mar", text = df['valor_mediano_da_casa']
-                            ,mapbox_style="open-street-map", zoom=3, color_discrete_sequence = colors,
-                            height=800)
+
+    latitude_central, longitude_central = (df["latitude"].mean(), df["longitude"].mean())
+
+    fig = px.scatter_mapbox(df, lat="latitude", lon="longitude", color = "Proximidade do mar", text = df['valor mediano da casa']
+                            ,mapbox_style="open-street-map", zoom=5, color_discrete_sequence = colors,
+                            height=800, width = 1000, center = dict(lat=latitude_central, lon=longitude_central))
 
     # Adicione o gráfico ao Streamlit
     st.plotly_chart(fig)
